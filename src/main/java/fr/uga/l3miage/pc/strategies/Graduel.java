dc8f7f37f80ce7b56acedb23445172ddd1bfd9bb
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-11-03 18:22:36 +0100

    feat: add all strategies but let them unimplemented

---
+++ src/main/java/fr/uga/l3miage/pc/strategies/Graduel.java
@@ -0,0 +1,12 @@
+package fr.uga.l3miage.pc.strategies;
+
+import fr.uga.l3miage.pc.enums.EnumIdJoueur;
+import fr.uga.l3miage.pc.models.Tour;
+
+public class Graduel implements SimpleStrategy {
+
+    @Override
+    public boolean doStrategy(Tour[] historique, EnumIdJoueur idJoueur) {
+        return true;
+    }
+}
