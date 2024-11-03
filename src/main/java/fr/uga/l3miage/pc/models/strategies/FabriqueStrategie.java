af930e2af3b3f0d27baf97c00f1a03ffe45239eb
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-11-03 16:21:45 +0100

    ref: improve readability of FabriqueStrategie.createStrategie

--- src/main/java/fr/uga/l3miage/pc/models/strategies/FabriqueStrategie.java
+++ src/main/java/fr/uga/l3miage/pc/models/strategies/FabriqueStrategie.java
@@ -6,6 +6,7 @@ import fr.uga.l3miage.pc.enums.EnumTechniquesAuto;
 public class FabriqueStrategie {
 
     private static FabriqueStrategie instance;
+
     private FabriqueStrategie(){}
 
     public static FabriqueStrategie getInstance() {
@@ -15,20 +16,14 @@ public class FabriqueStrategie {
         return instance;
     }
 
-    public SimpleStrategy createShape(EnumTechniquesAuto techniqueAuto) {
-        switch (techniqueAuto) {
-            case DONNANT_DONNANT:
-                return new DonnantDonnant();
-            case DONNANT_DONNANT_ALEATOIRE:
-                return new DonnantDonnantAleatoire();
-            case SONDEUR_NAIF:
-                return new SondeurNaif();
-            case TJRS_COOP:
-                return new ToujoursCooperer();
-            case TJRS_TRAHIR:
-                return new ToujoursTrahir();
-            default:
-                return null;
-        }
+    public SimpleStrategy createStrategie(EnumTechniquesAuto techniqueAuto) {
+        return switch (techniqueAuto) {
+            case DONNANT_DONNANT -> new DonnantDonnant();
+            case DONNANT_DONNANT_ALEATOIRE -> new DonnantDonnantAleatoire();
+            case SONDEUR_NAIF -> new SondeurNaif();
+            case TJRS_COOP -> new ToujoursCooperer();
+            case TJRS_TRAHIR -> new ToujoursTrahir();
+            default -> null;
+        };
     }
 }
