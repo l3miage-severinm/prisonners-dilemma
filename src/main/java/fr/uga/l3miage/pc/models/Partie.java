4d046131e56e8c466b5559937b8e6ac694de1392
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-11-03 13:02:11 +0100

    feat: implement Partie.estFinie()

--- src/main/java/fr/uga/l3miage/pc/models/Partie.java
+++ src/main/java/fr/uga/l3miage/pc/models/Partie.java
@@ -9,4 +9,6 @@ public class Partie {
     private final int numero;
     private final int nbTours;
     private final List<Tour> tours;
+
+    public boolean estFinie() { return tours.size() == nbTours && tours.get(tours.size() - 1).estFini(); }
 }
