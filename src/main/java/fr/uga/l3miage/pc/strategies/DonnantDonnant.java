6ae3c32a3094f8f83d9d2c041e728a185144ae97
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-11-04 20:11:48 +0100

    fix: bug on DonnantDonnant detected thanks to DonnantDonnantTest

--- src/main/java/fr/uga/l3miage/pc/strategies/DonnantDonnant.java
+++ src/main/java/fr/uga/l3miage/pc/strategies/DonnantDonnant.java
@@ -7,7 +7,7 @@ public class DonnantDonnant implements SimpleStrategy {
 
     @Override
     public boolean doStrategy(Tour[] historique, EnumIdJoueur idJoueur) {
-        return (idJoueur == EnumIdJoueur.MILOU) ?
+        return (idJoueur == EnumIdJoueur.TINTIN) ?
                 historique[historique.length - 1].getJoueur2Coopere() :
                 historique[historique.length - 1].getJoueur1Coopere();
     }
