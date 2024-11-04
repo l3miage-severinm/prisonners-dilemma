a06a2054f092cb9d67129dcd960ba5a3ba5c49ae
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-11-04 20:12:50 +0100

    ref: add annotations about constructor on Tour in order to facilitate testing

--- src/main/java/fr/uga/l3miage/pc/models/Tour.java
+++ src/main/java/fr/uga/l3miage/pc/models/Tour.java
@@ -1,8 +1,12 @@
 package fr.uga.l3miage.pc.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tour {
     private Boolean joueur1Coopere;
     private Boolean joueur2Coopere;
