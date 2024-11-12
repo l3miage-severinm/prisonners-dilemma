46bdcb77011df3f5cd7f2176fe4b5956c5ada799
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-11-12 19:16:03 +0100

    feat: test and implement strategy "Graduel"

--- src/main/java/fr/uga/l3miage/pc/strategies/Graduel.java
+++ src/main/java/fr/uga/l3miage/pc/strategies/Graduel.java
@@ -7,6 +7,43 @@ public class Graduel implements SimpleStrategy {
 
     @Override
     public boolean doStrategy(Tour[] historique, EnumIdJoueur idJoueur) {
-        return true;
+
+        if (calculerTrahisonsRestantes(historique, idJoueur) > 0)
+            return false;
+
+        if (calculerCooperationsRestantes(historique, idJoueur) > 0)
+            return true;
+
+        return ! hasAdversaireTrahiDernierTour(historique, idJoueur);   // retourner false si l'adversaire a trahi
+    }
+
+    private int countAdversaireTrahisons(Tour[] historique, EnumIdJoueur idJoueur) {
+        int count = 0;
+        for (Tour tour : historique) {
+            if (idJoueur == EnumIdJoueur.TINTIN && Boolean.FALSE.equals(tour.getJoueur2Coopere()))
+                count++;
+            else if (idJoueur == EnumIdJoueur.MILOU && Boolean.FALSE.equals(tour.getJoueur1Coopere()))
+                count++;
+        }
+        return count;
+    }
+
+    private int calculerTrahisonsRestantes(Tour[] historique, EnumIdJoueur idJoueur) {
+        return hasAdversaireTrahiDernierTour(historique, idJoueur) ?
+                countAdversaireTrahisons(historique, idJoueur) :
+                0;
+    }
+
+    private int calculerCooperationsRestantes(Tour[] historique, EnumIdJoueur idJoueur) {
+        return hasAdversaireTrahiDernierTour(historique, idJoueur) ? 2 : 0;
+    }
+
+    private boolean hasAdversaireTrahiDernierTour(Tour[] historique, EnumIdJoueur idJoueur) {
+        if (historique.length == 0)
+            return false;
+
+        Tour dernierTour = historique[historique.length - 1];
+        return (idJoueur == EnumIdJoueur.TINTIN && Boolean.FALSE.equals(dernierTour.getJoueur2Coopere())) ||
+                (idJoueur == EnumIdJoueur.MILOU && Boolean.FALSE.equals(dernierTour.getJoueur1Coopere()));
     }
 }
