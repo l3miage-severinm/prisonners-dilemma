4c0cbd27dd6567565821d91d693d259bf5c0b3ce
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-11-12 21:44:19 +0100

    feat: test and implement strategy "Sondeur repentant"

--- src/main/java/fr/uga/l3miage/pc/strategies/SondeurRepentant.java
+++ src/main/java/fr/uga/l3miage/pc/strategies/SondeurRepentant.java
@@ -3,10 +3,34 @@ package fr.uga.l3miage.pc.strategies;
 import fr.uga.l3miage.pc.enums.EnumIdJoueur;
 import fr.uga.l3miage.pc.models.Tour;
 
+import java.util.Random;
+
 public class SondeurRepentant implements SimpleStrategy {
+
+    private final Random random = new Random();
 
     @Override
     public boolean doStrategy(Tour[] historique, EnumIdJoueur idJoueur) {
-        return true;
+        if (historique.length == 0)
+            return true;
+
+        Tour dernierTour = historique[historique.length - 1];
+        boolean dernierCoupAdversaireCoopere = idJoueur == EnumIdJoueur.TINTIN ?
+                dernierTour.getJoueur2Coopere() :
+                dernierTour.getJoueur1Coopere();
+
+        if (dernierCoupAdversaireCoopere && random.nextInt(100) >= 50)  // probabilité de trahir de 1/2
+            return false;
+
+        if (historique.length > 1 && !dernierCoupAdversaireCoopere) {
+            Tour avantDernierTour = historique[historique.length - 2];
+            boolean avantDernierCoupJoueurCoopere = idJoueur == EnumIdJoueur.TINTIN ?
+                    avantDernierTour.getJoueur1Coopere() :
+                    avantDernierTour.getJoueur2Coopere();
+            if (!avantDernierCoupJoueurCoopere)
+                return true;
+        }
+
+        return dernierCoupAdversaireCoopere;
     }
 }
