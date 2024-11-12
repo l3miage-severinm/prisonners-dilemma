4e146e0bc608e2a33b22bd37b23ce36fe001b034
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-11-12 17:51:06 +0100

    feat: implement strategy "Aleatoire"

--- src/main/java/fr/uga/l3miage/pc/strategies/Aleatoire.java
+++ src/main/java/fr/uga/l3miage/pc/strategies/Aleatoire.java
@@ -3,10 +3,14 @@ package fr.uga.l3miage.pc.strategies;
 import fr.uga.l3miage.pc.enums.EnumIdJoueur;
 import fr.uga.l3miage.pc.models.Tour;
 
+import java.util.Random;
+
 public class Aleatoire implements SimpleStrategy {
+
+    private final Random random = new Random();
 
     @Override
     public boolean doStrategy(Tour[] historique, EnumIdJoueur idJoueur) {
-        return true;
+        return random.nextBoolean();
     }
 }
