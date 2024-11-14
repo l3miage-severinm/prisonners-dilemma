b7c4bf5b555f24a7ce3144857df6061a5399f58b
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-11-14 22:32:12 +0100

    fix: rename an exception which had a typo

--- src/main/java/fr/uga/l3miage/pc/exceptions/handlers/JoueurAPasJoueHandler.java
+++ src/main/java/fr/uga/l3miage/pc/exceptions/handlers/JoueurAPasJoueHandler.java
@@ -1,14 +1,14 @@
 package fr.uga.l3miage.pc.exceptions.handlers;
 
-import fr.uga.l3miage.pc.exceptions.rest.JoueurADejaJoueRestException;
+import fr.uga.l3miage.pc.exceptions.rest.JoueurAPasJoueRestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class JoueurAPasJoueHandler {
-    @ExceptionHandler(JoueurADejaJoueRestException.class)
-    public ResponseEntity<String> handle(JoueurADejaJoueRestException e){
+    @ExceptionHandler(JoueurAPasJoueRestException.class)
+    public ResponseEntity<String> handle(JoueurAPasJoueRestException e){
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
--- src/main/java/fr/uga/l3miage/pc/exceptions/rest/JoueurAPasJoueRestExecption.java
+++
@@ -1,7 +0,0 @@
-package fr.uga.l3miage.pc.exceptions.rest;
-
-public class JoueurAPasJoueRestExecption extends RuntimeException {
-    public JoueurAPasJoueRestExecption(String message) {
-        super(message);
-    }
-}
