3f6961a3d8f16043819bfa01cfe1bf00f4053ce5
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-12-04 09:47:52 +0100

    feat: wrap random strategy from group 26

--- src/main/java/fr/uga/l3miage/pc/strategies_g26/Aleatoire_g26.java
+++ src/main/java/fr/uga/l3miage/pc/strategies_g26/Aleatoire_g26.java
@@ -4,11 +4,12 @@ import fr.uga.l3miage.pc.enums.EnumIdJoueur;
 import fr.uga.l3miage.pc.models.Tour;
 import fr.uga.l3miage.pc.strategies.SimpleStrategy;
 import org.apache.commons.lang3.NotImplementedException;
+import org.dillemaprisonner.ugamiage12024group2_6.strategies.RandomStrategy;
 
 public class Aleatoire_g26 implements SimpleStrategy {
 
     @Override
     public boolean doStrategy(Tour[] historique, EnumIdJoueur idJoueur) {
-        throw new NotImplementedException();
+        return Utils_g26.DecisionToBoolean(new RandomStrategy().nextMove());
     }
 }
